b12bfa102e9d3247fdb8c0b0a22f2c14926c70c4
James-Prentice <Jamesprenticez@gmail.com>
2021-01-16 12:53:11 +1300

    ControlledInput

--- client/components/MyComponent.jsx
+++ client/components/MyComponent.jsx
@@ -4,13 +4,22 @@ export default class MyComponent extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            input: ''
         }
+    this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event){
+        this.setState({
+            input: event.target.value
+        })
     }
 
     render() {
         return (
             <>
+                <input value={this.state.input} onChange={this.handleChange}></input>
+                <p><strong>Controlled input:</strong> {this.state.input}</p>
             </>
         )
     }
