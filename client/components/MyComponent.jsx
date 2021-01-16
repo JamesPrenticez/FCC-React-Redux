075ca88c0953bc54982208a16b19087556b72c7a
James-Prentice <Jamesprenticez@gmail.com>
2021-01-16 13:13:52 +1300

    Controlled Fomr

--- client/components/MyComponent.jsx
+++ client/components/MyComponent.jsx
@@ -4,14 +4,39 @@ export default class MyComponent extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            input: '',
+            submit: ''
         }
-
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
+
+    handleChange(event){
+        this.setState({
+            input: event.target.value
+        })
+    }
+
+    handleSubmit(event){
+        event.preventDefault()
+        this.setState({
+        submit: this.state.input    
+        })
+    }
+    
 
     render() {
         return (
             <>
+            <form onSubmit={this.handleSubmit}>
+            
+            <input value={this.state.input} onChange = {this.handleChange.bind(this)}></input>
+
+            <button type='submit'>Submit</button>
+            </form>
+            
+            <p><strong>Submit Value:</strong> {this.state.submit}</p>
+
             </>
         )
     }
