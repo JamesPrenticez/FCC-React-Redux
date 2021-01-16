2e27de749d8a303cae8f5bd7b32e064be4a0ac70
James-Prentice <Jamesprenticez@gmail.com>
2021-01-17 11:59:33 +1300

    Array Map ToDos

--- client/components/MyComponent.jsx
+++ client/components/MyComponent.jsx
@@ -1,18 +1,49 @@
 import React from 'react'
 
-export default class MyComponent extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
+const textAreaStyles = {
+  width: 235,
+  margin: 5
+};
 
-        }
-
+export default class MyToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    // Change code below this line
+    this.state = {
+      input: '',
+      toDoList: []
     }
-
-    render() {
-        return (
-            <>
-            </>
-        )
-    }
+    // Change code above this line
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit() {
+    const itemsArray = this.state.userInput.split(',');
+    this.setState({
+      toDoList: itemsArray
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value
+    });
+  }
+  render() {
+    const items = this.state.toDoList.map(task => <li>{task}</li>)
+    return (
+      <div>
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.userInput}
+          style={textAreaStyles}
+          placeholder='Separate Items With Commas'
+        />
+        <br />
+        <button onClick={this.handleSubmit}>Create List</button>
+        <h1>My "To Do" List:</h1>
+        <ul>{items}</ul>
+      </div>
+    );
+  }
 }
+
