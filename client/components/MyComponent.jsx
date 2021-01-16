3e23c3e9bcf476a0d04ccc2a4764e263dcfcad04
James-Prentice <Jamesprenticez@gmail.com>
2021-01-17 12:15:00 +1300

    Users online array of objects

--- client/components/MyComponent.jsx
+++ client/components/MyComponent.jsx
@@ -1,18 +1,45 @@
 import React from 'react'
 
 export default class MyComponent extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          username: 'Jeff',
+          online: true
+        },
+        {
+          username: 'Alan',
+          online: false
+        },
+        {
+          username: 'Mary',
+          online: true
+        },
+        {
+          username: 'Jim',
+          online: false
+        },
+        {
+          username: 'Sara',
+          online: true
+        },
+        {
+          username: 'Laura',
+          online: true
         }
-
-    }
-
-    render() {
-        return (
-            <>
-            </>
-        )
-    }
+      ]
+    };
+  }
+  render() {
+    const usersOnline = this.state.users.filter(index => index.online == true)
+    const renderOnline = usersOnline.map(index => <li key={index.username + 1}>{index.username}</li>)
+    return (
+      <div>
+        <h1>Current Online Users:</h1>
+        <ul>{renderOnline}</ul>
+      </div>
+    );
+  }
 }
