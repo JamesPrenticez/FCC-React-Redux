f05f47ef57040d8c48db62d878b2da11e48d5608
James-Prentice <Jamesprenticez@gmail.com>
2021-01-16 13:35:46 +1300

    NameToNavbar

--- client/components/MyComponent.jsx
+++ client/components/MyComponent.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
 
-export default class MyComponent extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-
-        }
-
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'James'
     }
+  }
+  render() {
+    return (
+       <div>
+         <Navbar name= {this.state.name}/>
+       </div>
+    );
+  }
+};
 
-    render() {
-        return (
-            <>
-            </>
-        )
-    }
-}
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+    <div>
+      {/* Change code below this line */}
+      <h1>Hello, my name is: {this.props.name}</h1>
+      {/* Change code above this line */}
+    </div>
+    );
+  }
+};
+
+export default MyComponent
